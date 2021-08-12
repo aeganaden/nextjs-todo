5afb4233c1b7c3878910b9154cff8ea033b7cdc7
Angelo Ganaden Jr <angelojrganaden@gmail.com>
2021-08-12 21:33:14 +0800

    style: add tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend : {
+      colors: {
+        primary: {
+          light: '#a16144',
+          DEFAULT: '#d99433',
+          dark: '#6e2e11'
+        },
+        secondary: {
+          light: '#e1a95c',
+          DEFAULT: '#8a3915',
+          dark: '#ae7629'
+        }
+      }
+    }
   },
   variants: {
     extend: {},

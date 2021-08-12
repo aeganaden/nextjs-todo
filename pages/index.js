f30a54b66d9e171989b1dce08480334cb440566e
Angelo Ganaden Jr <angelojrganaden@gmail.com>
2021-08-12 17:52:15 +0800

    test: gpg signing

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.scss'
 export default function Home() {
   return (
     <div>
-      <div className='text-xs text-gray-600 uppercase font-bold'>hello</div>
+      <div className='text-xs text-gray-600 uppercase font-bold'>gpg test</div>
     </div>
   )
 }
